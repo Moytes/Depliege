b637ccd9c585cca691729f89808b2536f63c86a6
Moytes <moisesgodinezvenegas17@gmail.com>
2025-11-11 01:01:06 -0600

    prueva 5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { App as AntApp } from 'antd';
 
 import { CssBaseline } from '@mui/material';
+// Usamos BrowserRouter, ¡está perfecto!
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { ResponsiveView } from './views/landing/Inicio/ResponsiveView';
@@ -22,47 +23,47 @@ import { ProtectedRoute } from './types/routes/ProtectedRoute';
 import { TermsView } from './views/auth/terminos/TermsView';
 
 function App() {
-    return (
-        <AntApp>
-            <CssBaseline />
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/admin"
-                        element={
-                            <ProtectedRoute>
-                                <AdminLayout />
-                            </ProtectedRoute>
-                        }
-                    >
-                        <Route index element={<Navigate to="dashboard" replace />} />
-                        <Route path="dashboard" element={<AdminWelcomeView />} />
-                        <Route path="profile" element={<AdminProfileView />} />
-                        <Route path="users" element={<UserManagementView />} />
-                    </Route>
+    return (
+        <AntApp>
+            <CssBaseline />
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/admin"
+                        element={
+                            <ProtectedRoute>
+                                <AdminLayout />
+                            </ProtectedRoute>
+                        }
+                    >
+                        <Route index element={<Navigate to="dashboard" replace />} />
+                        <Route path="dashboard" element={<AdminWelcomeView />} />
+                        <Route path="profile" element={<AdminProfileView />} />
+                        <Route path="users" element={<UserManagementView />} />
+                    </Route>
 
-                    <Route
-                        path="/user"
-                        element={
-                            <ProtectedRoute>
-                                <UserLayout />
-                            </ProtectedRoute>
-                        }
-                    >
-                        <Route index element={<Navigate to="welcome" replace />} />
-                        <Route path="welcome" element={<WelcomeUserView />} />
-                        <Route path="profile" element={<UserProfileView />} />
-                        <Route path="gestion-invernadero" element={<GestionInvernaderoView />} />
-                        <Route path="gestion-invernadero/frio" element={<GestionInvernaderoFrioView />} />
-                        <Route path="gestion-invernadero/calido" element={<GestionInvernaderoCalidoView />} />
-                    </Route>        
-                    <Route path="/terminos" element={<TermsView />} />
-                    
-                    <Route path="/*" element={<ResponsiveView />} />
-                </Routes>
-            </BrowserRouter>
-        </AntApp>
-    );
+                    <Route
+                        path="/user"
+                        element={
+                            <ProtectedRoute>
+                                <UserLayout />
+                            </ProtectedRoute>
+                        }
+                    >
+                        <Route index element={<Navigate to="welcome" replace />} />
+                        <Route path="welcome" element={<WelcomeUserView />} />
+                        <Route path="profile" element={<UserProfileView />} />
+                        <Route path="gestion-invernadero" element={<GestionInvernaderoView />} />
+                        <Route path="gestion-invernadero/frio" element={<GestionInvernaderoFrioView />} />
+                        <Route path="gestion-invernadero/calido" element={<GestionInvernaderoCalidoView />} />
+f                  </Route>        
+                    <Route path="/terminos" element={<TermsView />} />
+                    
+                    <Route path="/*" element={<ResponsiveView />} />
+                </Routes>
+            </BrowserRouter>
+        </AntApp>
+    );
 }
 
 export default App;
